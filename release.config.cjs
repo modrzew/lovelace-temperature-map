5f22ee70d5985c0a72abd91c30b7d9f41173ea29
Steven Sinatra <steven@stevensinatra.id>
2023-08-27 00:37:04 +1000

    ci: more semantic release fixes

--- release.config.cjs
+++ release.config.cjs
@@ -6,6 +6,10 @@ module.exports = {
         assets: 'dist/custom-cards.js',
       },
     ],
+    ['@semantic-release/commit-analyzer'],
+    {
+      preset: 'angular',
+    },
   ],
   branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main'],
 };
